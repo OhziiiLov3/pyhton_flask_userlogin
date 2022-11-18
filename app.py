bbe6b4ef6c61cf996af35989e2e7091e5ebab614
ohziiilov3 <klbaskerville0520@gmail.com>
2022-11-18 08:28:00 -0800

    db: config pymongo, and insert model in db

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask , render_template
+import pymongo
+
+app = Flask(__name__)
+
+# Database Config 
+
+client = pymongo.MongoClient('localhost',27017)
+db = client.user_login_system
+
+# Routes 
+from user import routes
+
+@app.route('/')
+def home():
+  return render_template('home.html')
+
+
+@app.route('/dashboard/')
+def dashboard():
+  return render_template('dashboard.html')
---
+++ static/js/scripts.js
@@ -0,0 +1,21 @@
+$('form[name=signup_form').submit(function(e) {
+
+    let $form =$(this);
+    let $error = $form.find(".error");
+    let data = $form.serialize();
+
+    $.ajax({
+        url: "/user/signup",
+        type: "POST",
+        data: data,
+        dataType: "json",
+        success: function(res){
+            console.log(res);
+        },
+        error: function(res) {
+            console.log(res);
+        }
+    });
+
+  e.preventDefault();  
+})
